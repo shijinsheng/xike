219f5d9c9ff5844b099e8d5de507d65ded9f10e6
shijinsheng <1031735400@qq.com>
2014-09-27 22:18:25 +0800

    删除文件

--- dog.php
+++
@@ -1,8 +0,0 @@
-<?php
-//除header()外都是改的
-session_start();
-ob_start();
-$_SESSION['guanyuan']=md5('1234adas');
-header('Location: index.php?g=Admin&m=Login');
-ob_end_flush();
-?>
--- shijinsheng.php
+++
@@ -1,5 +0,0 @@
-add file shijinsheng.php.
-this is a master update.
-This is the issue_101 update.
-This is the master update.
-update master content  -> shijinsheng.php
